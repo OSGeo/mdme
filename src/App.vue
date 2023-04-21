73fa1824d27d8d0d69b3ca091d0fbdea17a221df
Paul van Genuchten <genuchten@yahoo.com>
2023-04-21 10:28:17 +0200

    export to iso, using demo.pygeoapi.io, fixes #5

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,8 @@ import VJsf from '@koumoul/vjsf/lib/VJsf.js'
 import '@koumoul/vjsf/lib/VJsf.css'
 import '@koumoul/vjsf/lib/deps/third-party.js'
 import MainMenu from './components/MainMenu.vue'
-import { stringify  } from "yaml";
+import { stringify } from "yaml";
+
 const vocab = "mcf"; //ogcapi-record
 
 //import model
@@ -47,6 +48,16 @@ export default {
     reset () {
       this.model = {};
     },
+    download (data,mode) {
+      const blob = new Blob([data], {type: 'text/plain'})
+      const e = document.createEvent('MouseEvents'),
+      a = document.createElement('a');
+      a.download = this.model.metadata.identifier + "." + mode;
+      a.href = window.URL.createObjectURL(blob);
+      a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
+      e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+      a.dispatchEvent(e);
+    },
     setModel (mdl) {
       //copy key-values from object 2 levels deep
       let self = this;
@@ -58,7 +69,11 @@ export default {
         }
       })
     },
-    saveData() {
+    saveData(mode) {
+      let self = this;
+      if (!mode){
+        mode = 'mcf';
+      }
       let toExp = Object.assign({}, this.model);
       //convert contact/dist to objects
       var cnt = {}
@@ -76,17 +91,32 @@ export default {
         })
       }
       toExp['distribution'] = dst;
-      const data = stringify(toExp)
-      const blob = new Blob([data], {type: 'text/plain'})
-      const e = document.createEvent('MouseEvents'),
-      a = document.createElement('a');
-      a.download = this.model.metadata.identifier + ".yml";
-      a.href = window.URL.createObjectURL(blob);
-      a.dataset.downloadurl = ['text/vnd.yaml', a.download, a.href].join(':');
-      e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-      a.dispatchEvent(e);
+
+      if (mode=='mcf'){
+        self.download(stringify(toExp), 'yml');
+      } else {
+        this.axios.post("https://demo.pygeoapi.io/master/processes/pygeometa-metadata-generate/execution", {
+          "inputs": {
+            "mcf": toExp,
+            "schema": mode
+          }
+        },{
+          headers: {'Accept': 'application/json'}
+        }).then(function(response){
+          //todo: build some check to evaluate if the content is of expected type
+
+          if (typeof response.data === 'object'){
+            self.download(JSON.stringify(response.data), 'json');
+          } else {
+            self.download(response.data, 'xml');
+          }
+        }).catch(function (error) {
+          alert(error);
+        })
+      }
     }
   }
 };
+
 </script>
 
